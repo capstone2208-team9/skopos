a47a3eaf5070a34579bd735fa2527999331c32ae
Gagan Sapkota <here.gagan@gmail.com>
2022-11-11 15:53:41 -0800

    changed fargate to Ec2 and added capacity
    Co-Authored-By: Katherine Ebel <kathyebel@me.com>
    Co-Authored-By: Nykaela Dodson <nykaelad@gmail.com>
    Co-Authored-By: Hans Elde <hanserik.elde@gmail.com>

--- lib/ecs/ecs-stack.ts
+++ lib/ecs/ecs-stack.ts
@@ -1,14 +1,22 @@
-import * as cdk from 'aws-cdk-lib'
-import {CfnOutput} from 'aws-cdk-lib'
-import {SecurityGroup, Vpc} from 'aws-cdk-lib/aws-ec2'
-import {Cluster, ContainerImage,} from 'aws-cdk-lib/aws-ecs'
-import {ApplicationLoadBalancedFargateService} from 'aws-cdk-lib/aws-ecs-patterns'
-import {ApplicationProtocol} from 'aws-cdk-lib/aws-elasticloadbalancingv2'
-import {Effect, PolicyStatement,} from 'aws-cdk-lib/aws-iam'
-import * as lambda from 'aws-cdk-lib/aws-lambda'
-import {DatabaseInstance} from 'aws-cdk-lib/aws-rds'
-import {Construct} from 'constructs'
-import * as path from 'path'
+import * as cdk from "aws-cdk-lib";
+import { CfnOutput } from "aws-cdk-lib";
+import {
+  SecurityGroup,
+  Vpc,
+  InstanceType,
+  SubnetType,
+} from "aws-cdk-lib/aws-ec2";
+import {
+  Cluster,
+  ContainerImage,
+} from "aws-cdk-lib/aws-ecs";
+import { ApplicationLoadBalancedEc2Service } from "aws-cdk-lib/aws-ecs-patterns";
+import { ApplicationProtocol } from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { DatabaseInstance } from "aws-cdk-lib/aws-rds";
+import { Construct } from "constructs";
+import * as path from "path";
 
 export interface EcsStackProps extends cdk.StackProps {
   db: DatabaseInstance;
@@ -17,28 +25,41 @@ export interface EcsStackProps extends cdk.StackProps {
 }
 
 export class EcsStack extends cdk.Stack {
-  readonly backendFargateService: ApplicationLoadBalancedFargateService
-  readonly collectionRunnerFargateService: ApplicationLoadBalancedFargateService
+  readonly backendEc2Service: ApplicationLoadBalancedEc2Service;
+  readonly collectionRunnerEc2Service: ApplicationLoadBalancedEc2Service;
 
   constructor(scope: Construct, id: string, props: EcsStackProps) {
-    super(scope, id, props)
-    const {vpc, db, dbCredentials} = props
+    super(scope, id, props);
+    const { vpc, db, dbCredentials } = props;
 
-    const cluster = new Cluster(this, 'ECSCluster', {
+    const cluster = new Cluster(this, "ECSCluster", {
       vpc,
       containerInsights: true,
-    })
+    });
 
-    const func = new lambda.Function(this, 'call-collection-runner', {
+    //add capacity to the cluster
+    cluster.addCapacity("DefaultAutoScalingGroup", {
+      instanceType: new InstanceType("t3.micro"),
+      desiredCapacity: 2,
+      minCapacity: 2,
+      maxCapacity: 3,
+      vpcSubnets: {
+        subnetType: SubnetType.PUBLIC,
+      },
+    });
+
+    const func = new lambda.Function(this, "call-collection-runner", {
       runtime: lambda.Runtime.NODEJS_16_X,
       memorySize: 1024,
       timeout: cdk.Duration.seconds(5),
-      handler: 'index.collectionRunner',
-      code: lambda.Code.fromAsset(path.join(__dirname, './../../src/lambda-function')),
+      handler: "index.collectionRunner",
+      code: lambda.Code.fromAsset(
+        path.join(__dirname, "./../../src/lambda-function")
+      ),
       environment: {
         REGION: cdk.Stack.of(this).region,
         AVAILABILITY_ZONES: JSON.stringify(
-          cdk.Stack.of(this).availabilityZones,
+          cdk.Stack.of(this).availabilityZones
         ),
       },
     });
@@ -53,118 +74,96 @@ export class EcsStack extends cdk.Stack {
     //   }
     // );
 
-
-
-
-    this.backendFargateService = new ApplicationLoadBalancedFargateService(this, 'BackendFargateService', {
-      cluster,
-      publicLoadBalancer: true,
-      openListener: true,
-      assignPublicIp: true,
-      loadBalancerName: 'BackendLoadBalancerDNSName',
-      serviceName: 'BackendService',
-      desiredCount: 1,
-      memoryLimitMiB: 1024,
-      cpu: 512,
-      protocol: ApplicationProtocol.HTTP,
-      taskImageOptions: {
-        image: ContainerImage.fromRegistry('kat201/skopos-backend'),
-        containerPort: 3001,
-        containerName: 'BackendContainer',
-        enableLogging: true,
-        environment: {
-          DATABASE_URL: `postgresql://${dbCredentials.username}:${dbCredentials.password}@${db.dbInstanceEndpointAddress}:${db.dbInstanceEndpointPort}/prisma?schema=public&connect_timeout=60`,
-          LAMBDA_ARN: func.functionArn,
-          AWS_REGION: 'us-east-1'
+    this.backendEc2Service = new ApplicationLoadBalancedEc2Service(
+      this,
+      "BackendFargateService",
+      {
+        cluster,
+        publicLoadBalancer: true,
+        openListener: true,
+        loadBalancerName: "BackendLoadBalancerDNSName",
+        serviceName: "BackendService",
+        desiredCount: 1,
+        memoryLimitMiB: 1024,
+        cpu: 512,
+        protocol: ApplicationProtocol.HTTP,
+        taskImageOptions: {
+          image: ContainerImage.fromRegistry("ahamoudeis/backend_skopos:1.8"),
+          containerPort: 3001,
+          containerName: "BackendContainer",
+          enableLogging: true,
+          environment: {
+            DATABASE_URL: `postgresql://${dbCredentials.username}:${dbCredentials.password}@${db.dbInstanceEndpointAddress}:${db.dbInstanceEndpointPort}/prisma?schema=public&connect_timeout=60`,
+            LAMBDA_ARN: func.functionArn,
+            AWS_REGION: "us-east-1",
+          },
         },
       }
-    })
-    // backendServiceSG.connections.allowFrom(this.backendFargateService.loadBalancer, Port.tcp(3001))
+    );
+    // backendServiceSG.connections.allowFrom(this.backendEc2Service.loadBalancer, Port.tcp(3001))
 
-    this.backendFargateService.taskDefinition.addToTaskRolePolicy(
+    this.backendEc2Service.taskDefinition.addToTaskRolePolicy(
       new PolicyStatement({
         effect: Effect.ALLOW,
-        resources: ['*'],
+        resources: ["*"],
         actions: [
-          'events:EnableRule',
-          'events:PutRule',
-          'sns:CreateTopic',
-          'sns:Unsubscribe',
-          'events:DeleteRule',
-          'events:PutTargets',
-          'sns:Publish',
-          'events:ListRuleNamesByTarget',
-          'events:ListRules',
-          'sns:Subscribe',
-          'events:RemoveTargets',
-          'events:ListTargetsByRule',
-          'events:DisableRule',
-          'lambda:AddPermission',
-          'lambda:RemovePermission',
+          "events:EnableRule",
+          "events:PutRule",
+          "sns:CreateTopic",
+          "sns:Unsubscribe",
+          "events:DeleteRule",
+          "events:PutTargets",
+          "sns:Publish",
+          "events:ListRuleNamesByTarget",
+          "events:ListRules",
+          "sns:Subscribe",
+          "events:RemoveTargets",
+          "events:ListTargetsByRule",
+          "events:DisableRule",
+          "lambda:AddPermission",
+          "lambda:RemovePermission",
         ],
       })
-    )
+    );
 
-    this.backendFargateService.targetGroup.configureHealthCheck({
-      port: '3001',
-      path: '/health'
-    })
-
-    const collectionRunnerServiceSG = new SecurityGroup(
+    this.collectionRunnerEc2Service = new ApplicationLoadBalancedEc2Service(
       this,
-      `collectionRunner-server-security-group`,
+      "CollectionRunnerFargateService",
       {
-        allowAllOutbound: true,
-        securityGroupName: `collectionRunner-server-security-group`,
-        vpc,
+        cluster,
+        publicLoadBalancer: true,
+        openListener: true,
+        loadBalancerName: "CollectionRunnerDNSName",
+        serviceName: "CollectionRunnerService",
+        desiredCount: 1,
+        memoryLimitMiB: 1024,
+        cpu: 512,
+        taskImageOptions: {
+          image: ContainerImage.fromRegistry("ahamoudeis/collection_runner_skopos:1.0"),
+          containerPort: 3003,
+          containerName: "CollectionRunnerContainer",
+          enableLogging: true,
+          environment: {
+            GRAPHQL_URL: `${this.backendEc2Service.loadBalancer.loadBalancerDnsName}/graphql`,
+            AWS_REGION: "us-east-1",
+          },
+        },
       }
     );
 
-    this.collectionRunnerFargateService = new ApplicationLoadBalancedFargateService(this, 'CollectionRunnerFargateService', {
-      cluster,
-      publicLoadBalancer: true,
-      openListener: true,
-      assignPublicIp: true,
-      loadBalancerName: 'CollectionRunnerDNSName',
-      serviceName: 'CollectionRunnerService',
-      desiredCount: 1,
-      memoryLimitMiB: 1024,
-      cpu: 512,
-      taskImageOptions: {
-        image: ContainerImage.fromRegistry('kat201/collection-runner'),
-        containerPort: 3003,
-        containerName: 'CollectionRunnerContainer',
-        enableLogging: true,
-        environment: {
-          GRAPHQL_URL: `${this.backendFargateService.loadBalancer.loadBalancerDnsName}/graphql`,
-          AWS_REGION: 'us-east-1',
-        }
-      }
-    })
-
     // collectionRunnerServiceSG.connections.allowFrom(this.collectionRunnerFargateService.loadBalancer, Port.tcp(3003))
 
+    func.addEnvironment(
+      "" + "COLLECTION_RUNNER_URI",
+      this.collectionRunnerEc2Service.loadBalancer.loadBalancerDnsName
+    );
 
-    this.collectionRunnerFargateService.targetGroup.configureHealthCheck({
-      port: '3003',
-      path: '/health',
+    new CfnOutput(this, "BackendLoadBalancerDNSName", {
+      value: this.backendEc2Service.loadBalancer.loadBalancerDnsName,
+    });
 
-    })
-
-    func.addEnvironment('' +
-      'COLLECTION_RUNNER_URI',
-      this.collectionRunnerFargateService.loadBalancer.loadBalancerDnsName,
-    )
-
-
-
-
-    new CfnOutput(this, 'BackendLoadBalancerDNSName', {
-      value: this.backendFargateService.loadBalancer.loadBalancerDnsName,
-    })
-
-    new CfnOutput(this, 'CollectionRunnerLoadBalancerDNSName', {
-      value: this.collectionRunnerFargateService.loadBalancer.loadBalancerDnsName,
-    })
+    new CfnOutput(this, "CollectionRunnerLoadBalancerDNSName", {
+      value: this.collectionRunnerEc2Service.loadBalancer.loadBalancerDnsName,
+    });
   }
 }
