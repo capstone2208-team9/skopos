0d3a9877d2f16fca72d22f1f6fcaeda8e7ed77c6
Katherine Ebel <kathyebel@me.com>
2022-11-17 08:43:00 -0500

    fix default export name
    Co-authored-by: Hans Elde <hanserik.elde@gmail.com>
    Co-authored-by: Nykaela Dodson <nykaelad@gmail.com>
    Co-authored-by: Gagan Sapkota <here.gagan@gmail.com>

--- packages/cli/src/commands/destroy.ts
+++ packages/cli/src/commands/destroy.ts
@@ -1,6 +1,6 @@
 import {executeCommand, ROOT_PATH} from '../lib/index.js'
 
-export default async function deploy() {
+export default async function destroy() {
   const command = 'npm run destroy --workspaces --if-present'
   await executeCommand(ROOT_PATH, command)
 }
